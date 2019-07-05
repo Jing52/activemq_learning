9e90f314035c880fda7281f4adb90bd4dffc22f6
nullcxy <1287530995@qq.com>
2019-07-05 16:59:37 +0800

    主题模式

---
+++ src/main/java/com/cxy/jms/topic/ActiveMqConsumer.java
@@ -0,0 +1,49 @@
+package com.cxy.jms.topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * @Auther: cxy
+ * @Date: 2019/7/4
+ * @Description: 消费者
+ */
+public class ActiveMqConsumer {
+    private static final String url = "tcp://192:168.31.10:61616";
+    private static final String topicName = "topic-test";
+
+    public static void main(String[] args) throws JMSException {
+        //1.创建ConnectionFactory
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+
+        //2.创建Connection
+        Connection connection = connectionFactory.createConnection();
+
+        //3.启动连接
+        connection.start();
+
+        //4.创建会话
+        Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+
+        //5.创建一个目标
+        Destination destination = session.createTopic(topicName);
+
+        //6.创建消费者
+        MessageConsumer consumer = session.createConsumer(destination);
+
+        //7.创建一个监听器
+        consumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    System.out.println("接收消息："+textMessage.getText());
+                }catch (JMSException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+}
